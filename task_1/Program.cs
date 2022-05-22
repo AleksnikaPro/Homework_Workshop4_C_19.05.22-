5d95acb837eb484845a666d08f641caf0e33ded3
Aleksandra Popova <alexnika@bk.ru>
2022-05-22 11:53:51 +0300

    added changes task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,8 +1,18 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
-int[] array = new int [8] {0, 1, 0, 0, 1, 1, 1, 0};
+
+// Вариант 1
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write($" {array[ i ]} ");
+}
+
+// Вариант 2
+/*int[] array = new int [8] {0, 1, 0, 0, 1, 1, 1, 0};
 int length = array.Length;
 for (int i = 0; i < length; i++)
 {
     Console.Write($" {array[ i ]} ");
-}
+}*/
